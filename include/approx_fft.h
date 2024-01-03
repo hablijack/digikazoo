e9ca4acc1a63fac0e1bcccdb6b6e17dab7c0b5a9
hablijack <christoph.habel@posteo.de>
2024-01-03 19:30:26 +0100

    feat: implemented approx_fft to speedup frequency detection

---
+++ include/approx_fft.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+float Approx_FFT(int in[], unsigned int N, float Frequency);
+
+int fast_sine(int Amp, int th);
+
+int fast_cosine(int Amp, int th);
+
+int fastRSS(int a, int b);
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
-#include "arduinoFFT.h"
+#include <Arduino.h>
+#include <approx_fft.h>
 
-#define SAMPLES 256              // SAMPLES-pt FFT. Must be a base 2 number. Max 128 for Arduino Uno.
-#define SAMPLING_FREQUENCY 24576 // Ts = Based on Nyquist, must be 2 times the highest expected frequency.
+#define SAMPLES 512              // SAMPLES-pt FFT. Must be a base 2 number. Max 128 for Arduino Uno.
+#define SAMPLING_FREQUENCY 24000 // Ts = Based on Nyquist, must be 2 times the highest expected frequency.
 #define log2(x) log(x) / log(2)
-
-arduinoFFT FFT = arduinoFFT();
 
 unsigned int samplingPeriod;
 unsigned long microSeconds;
 
-double vReal[SAMPLES]; // create vector of size SAMPLES to hold real values
-double vImag[SAMPLES]; // create vector of size SAMPLES to hold imaginary values
+int vReal[SAMPLES]; // create vector of size SAMPLES to hold real values
 
 unsigned long power;
 
@@ -29,13 +27,9 @@ void loop()
   /*Sample SAMPLES times*/
   for (int i = 0; i < SAMPLES; i++)
   {
-    microSeconds = micros(); // Returns the number of microseconds since the Arduino board began running the current script.
-
+    microSeconds = micros();  // Returns the number of microseconds since the Arduino board began running the current script.
     vReal[i] = analogRead(0); // Reads the value from analog pin 0 (A0), quantize it and save it as a real term.
-    vImag[i] = 0;             // Makes imaginary term 0 always
-
     power += vReal[i];
-
     /*remaining wait time between samples if necessary*/
     while (micros() < (microSeconds + samplingPeriod))
     {
@@ -45,13 +39,8 @@ void loop()
 
   unsigned long threshold = 0;
 
-  /*Perform FFT on samples*/
-  FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-  FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
-  FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
-
   /*Find peak frequency and print peak*/
-  double peak = FFT.MajorPeak(vReal, SAMPLES, SAMPLING_FREQUENCY);
+  float peak = Approx_FFT(vReal, SAMPLES, SAMPLING_FREQUENCY);
   int midi_num = round(12.0 * log2(peak / 440.0) + 69.0);
 
   if (power >= threshold)
