fcb14f27bd22f8d1b35e0ca9869e90732e741d4d
hablijack <christoph.habel@posteo.de>
2024-01-06 16:44:36 +0100

    feat: implemented midi velocity + power translation to expression

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,14 @@
 #define THRESHOLD 0
 #define SAMPLING_PERIOD round(1000000 * (1.0 / SAMPLING_FREQUENCY)) // Period in microseconds
 #define MIDI_CHANNEL 0
+#define MAX_POWER SAMPLES * 1023 // SAMPLES * ANALOG_READ MAX VALUE = max power possible
 
 unsigned long microSeconds;
 unsigned long power;
 unsigned int old_midi_note = 0;
 unsigned int midi_note;
+unsigned int old_midi_power = 0;
+unsigned int midi_power;
 float peak;
 
 int vReal[SAMPLES]; // create vector of size SAMPLES to hold real values
@@ -43,18 +46,27 @@ void loop()
 
   if (power >= THRESHOLD)
   {
+    midi_power = map(power, 0, MAX_POWER, 0, 127);
+    if (midi_power != old_midi_power)
+    {
+      Serial.println("... changing midi expression: " + midi_power);
+      BLEMidiServer.controlChange(MIDI_CHANNEL, 11, midi_power);
+      old_midi_power = midi_power;
+    }
+
     peak = Approx_FFT(vReal, SAMPLES, SAMPLING_FREQUENCY);
     midi_note = round(12.0 * log2(peak / 440.0) + 69.0);
     if (midi_note != old_midi_note)
     {
-      Serial.println("Note: " + notes[midi_note % 12] + ", Power: " + String(power));
+      Serial.println("... sending midi note: " + midi_note);
+      Serial.println("... playing note: " + notes[midi_note % 12]);
       BLEMidiServer.noteOff(MIDI_CHANNEL, old_midi_note, 127);
-      BLEMidiServer.noteOn(MIDI_CHANNEL, midi_note, 127);
+      BLEMidiServer.noteOn(MIDI_CHANNEL, midi_note, midi_power);
       old_midi_note = midi_note;
     }
   }
-  BLEMidiServer.noteOn(0, 69, 127);
-  delay(1000);
-  BLEMidiServer.noteOff(0, 69, 127); // Then we make a delay of one second before returning to the beginning of the loop
-  delay(1000);
+  else
+  {
+    BLEMidiServer.controlChange(MIDI_CHANNEL, 123, 0); // all notes off command
+  }
 }
